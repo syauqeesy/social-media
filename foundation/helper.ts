6fff5da117d9ba2d7012b4ee49a1cfc48e93ac2f
syauqeesy <syauqeesy@gmail.com>
2025-05-06 18:59:05 +0700

    add http error handler helper

--- foundation/helper.ts
+++ foundation/helper.ts
@@ -1,4 +1,8 @@
 import { Response } from "express";
+import { ValidationError } from "yup";
+import { HttpStatusCode } from "../enum/http-status-code";
+import { HttpStatusMessage } from "../enum/http-status-message";
+import HttpException from "../exception/exception";
 
 export const writeResponse = <Data>(
   response: Response,
@@ -10,4 +14,54 @@ export const writeResponse = <Data>(
     message,
     data,
   });
+};
+
+export const httpErrorHandler = (response: Response, error: unknown) => {
+  if (error instanceof ValidationError) {
+    console.warn(error.errors);
+
+    return writeResponse(
+      response,
+      HttpStatusCode.BadRequest,
+      HttpStatusMessage[HttpStatusCode.BadRequest],
+      error.errors
+    );
+  }
+
+  if (
+    error instanceof HttpException &&
+    error.getStatusCode() !== HttpStatusCode.InternalServerError
+  ) {
+    console.warn(error);
+
+    return writeResponse(
+      response,
+      error.getStatusCode(),
+      error.getMessage(),
+      null
+    );
+  }
+
+  if (
+    error instanceof HttpException &&
+    error.getStatusCode() === HttpStatusCode.InternalServerError
+  ) {
+    console.error(error);
+
+    return writeResponse(
+      response,
+      HttpStatusCode.InternalServerError,
+      HttpStatusMessage[HttpStatusCode.InternalServerError],
+      null
+    );
+  }
+
+  if (error instanceof Error) console.error(error);
+
+  writeResponse(
+    response,
+    HttpStatusCode.InternalServerError,
+    HttpStatusMessage[HttpStatusCode.InternalServerError],
+    null
+  );
 };
